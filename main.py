2853599c2bccd015442dd0f76617394115cb8c75
wallingson <128642980+wallingson12@users.noreply.github.com>
2024-05-06 10:59:29 -0300

    Add files via upload
    Modificação para que o usuário possa ajustar a posição da area de interesse de alerta

--- main.py
+++ main.py
@@ -3,19 +3,28 @@ import threading
 import pygame
 from datetime import datetime
 
-# Caminho do vídeo de entrada (substitua pelo caminho do seu arquivo de vídeo)
-Video_Path = 'ex01.mp4'
-
+# Caminho dos arquivos de configuração
 Config_Path = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
 Model_Path = 'frozen_inference_graph.pb'
 Classes_Path = 'coco.names'
+Video_Path = 'ex01.mp4'  # Caminho do vídeo de entrada (substitua pelo caminho do seu arquivo de vídeo)
+person_count = 0
 
-# Carregar a lista de classes
-with open(Classes_Path, 'r') as f:
-    ClassesList = f.read().splitlines()
+# Variáveis globais para controle da ROI
+roi_x = 250
+roi_y = 100
+roi_width = 500
+roi_height = 200
 
-# Definir o índice da classe desejada (pessoa)
-desired_class_index = ClassesList.index('person') + 1  # Encontrar o índice da classe 'person' (1-based index)
+# Variáveis globais para controle do redimensionamento
+resizing = False
+mouse_start = (0, 0)
+
+# Variáveis globais para controle do alerta e gravação de vídeo
+alerta_ativo = False
+alert_count = 0
+gravando_video = False
+video_writer = None
 
 # Inicializar o modelo de detecção
 net = cv2.dnn_DetectionModel(Config_Path, Model_Path)
@@ -24,13 +33,12 @@ net.setInputScale(1.0 / 127.5)
 net.setInputMean((127.5, 127.5, 127.5))
 net.setInputSwapRB(True)
 
-# Variável global para contar pessoas detectadas
-person_count = 0
+# Carregar a lista de classes
+with open(Classes_Path, 'r') as f:
+    ClassesList = f.read().splitlines()
 
-# Função para incrementar a contagem de pessoas detectadas
-def increment_person_count():
-    global person_count
-    person_count += 1
+# Definir o índice da classe desejada (pessoa)
+desired_class_index = ClassesList.index('person') + 1  # Encontrar o índice da classe 'person' (1-based index)
 
 # Inicializar o mixer de áudio do pygame
 pygame.mixer.init()
@@ -38,33 +46,49 @@ pygame.mixer.init()
 # Carregar o arquivo de áudio
 alert_sound = pygame.mixer.Sound('alert_sound.wav.mp3')
 
-# Variáveis para controle de alerta e gravação de vídeo
-alerta_ativo = False
-alert_count = 0
-gravando_video = False
-video_writer = None
+# Função de callback para eventos do mouse
+def mouse_callback(event, x, y, flags, param):
+    global resizing, roi_x, roi_y, mouse_start
 
-# Função para desenhar retângulo em uma área de interesse (ROI) com transparência
+    if event == cv2.EVENT_LBUTTONDOWN:
+        # Verificar se o clique está dentro da ROI e iniciar redimensionamento
+        if roi_x < x < roi_x + roi_width and roi_y < y < roi_y + roi_height:
+            resizing = True
+            mouse_start = (x, y)
+    elif event == cv2.EVENT_MOUSEMOVE:
+        # Atualizar a ROI durante o redimensionamento
+        if resizing:
+            delta_x = x - mouse_start[0]
+            delta_y = y - mouse_start[1]
+            roi_x += delta_x
+            roi_y += delta_y
+            mouse_start = (x, y)
+    elif event == cv2.EVENT_LBUTTONUP:
+        # Finalizar o redimensionamento ao soltar o botão do mouse
+        resizing = False
+
+# Função para desenhar retângulo na área de interesse (ROI) com transparência
 def draw_roi_rectangle(image):
-    # Quanto maior x mais a direita
-    # Quanto maior y mais abaixo
-    # Quanto maior w mais largo
-    # Quanto maior h mais alto
+    global roi_x, roi_y, roi_width, roi_height
 
-    x, y, w, h = 0, 200, 1000, 300  # Coordenadas da área de interesse (ROI)
     red_color = (0, 0, 255)  # Cor vermelha (BGR)
     transparency = 0.4  # Nível de transparência (0 a 1)
 
     overlay = image.copy()
-    cv2.rectangle(overlay, (x, y), (x + w, y + h), red_color, -1)  # Desenhar retângulo vermelho
+    cv2.rectangle(overlay, (roi_x, roi_y), (roi_x + roi_width, roi_y + roi_height), red_color, -1)  # Desenhar retângulo vermelho
     cv2.addWeighted(overlay, transparency, image, 1 - transparency, 0, image)
 
-    return (x, y, w, h)  # Retornar as coordenadas da ROI
+    return (roi_x, roi_y, roi_width, roi_height)  # Retornar as coordenadas da ROI
 
 # Função para verificar se um ponto (cx, cy) está dentro de uma área (x, y, w, h)
 def is_point_inside_area(cx, cy, area):
     x, y, w, h = area
     return x <= cx <= x + w and y <= cy <= y + h
+
+# Função para incrementar a contagem de pessoas detectadas
+def increment_person_count():
+    global person_count
+    person_count += 1
 
 # Função para iniciar a gravação de vídeo
 def iniciar_gravacao_video():
@@ -99,25 +123,14 @@ def emitir_alerta():
     alerta_ativo = False  # Desativar o alerta após o término
     alert_count = 0  # Zerar o contador de alerta após reprodução limitada
 
-# Inicializar a captura de vídeo do arquivo de vídeo
-video = cv2.VideoCapture(Video_Path)
-
-while True:
-    # Capturar o próximo frame do vídeo
-    ret, img = video.read()
-    if not ret:
-        break
-
-    # Redimensionar a imagem para processamento
-    img = cv2.resize(img, (1000, 700))
+# Função para processar os resultados da detecção
+def processar_deteccoes(classes, confidences, boxes, img):
+    global alerta_ativo, alert_count
 
     # Desenhar retângulo na área de interesse (ROI) com transparência
     area_of_interest = draw_roi_rectangle(img)
 
-    # Detectar objetos na imagem
-    classes, confidences, boxes = net.detect(img, confThreshold=0.6)
-
-    # Processar os resultados da detecção
+    # Iterar sobre as detecções
     if len(classes):
         for classId, confidence, box in zip(classes.flatten(), confidences.flatten(), boxes):
             if classId == desired_class_index and ClassesList[classId - 1] == 'person':
@@ -130,7 +143,6 @@ while True:
                 if is_point_inside_area(cx, cy, area_of_interest) and not alerta_ativo:
                     # Verificar se o alerta pode ser emitido novamente (máximo 3 vezes)
                     increment_person_count()
-                    print(person_count)
                     if alert_count < 3:
                         # Iniciar thread para reproduzir o alerta
                         threading.Thread(target=emitir_alerta).start()
@@ -139,6 +151,38 @@ while True:
                 # Desenhar retângulo ao redor da pessoa
                 cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
                 cv2.putText(img, 'person', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+
+# Função para inicializar a captura de vídeo
+def capturar_video():
+    return cv2.VideoCapture(Video_Path)
+
+# Função para liberar os recursos de vídeo
+def liberar_video(video):
+    if gravando_video:
+        encerrar_gravacao_video()
+    video.release()
+    cv2.destroyAllWindows()
+
+# Configurar a função de callback do mouse
+cv2.namedWindow('IMG')
+cv2.setMouseCallback('IMG', mouse_callback)
+
+# Inicializar a captura de vídeo
+video = capturar_video()
+
+# Loop principal
+while True:
+    # Capturar o próximo frame do vídeo
+    ret, img = video.read()
+    if not ret:
+        break
+
+    # Redimensionar a imagem para processamento
+    img = cv2.resize(img, (1000, 700))
+
+    # Processar os resultados da detecção
+    classes, confidences, boxes = net.detect(img, confThreshold=0.6)
+    processar_deteccoes(classes, confidences, boxes, img)
 
     # Gravar frame no vídeo se estiver gravando
     if gravando_video:
@@ -152,11 +196,9 @@ while True:
     cv2.imshow('IMG', img)
 
     # Aguardar pressionamento da tecla 'q' para sair
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord('q'):
         break
 
 # Liberar recursos
-if gravando_video:
-    encerrar_gravacao_video()
-video.release()
-cv2.destroyAllWindows()
+liberar_video(video)
